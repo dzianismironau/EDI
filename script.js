840e8f7db227cbff2cd0d9515ff7cb4e76805a13
dzianismironau <146941708+dzianismironau@users.noreply.github.com>
2024-01-06 13:46:25 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+function top_back() {
+    window.scrollTo(0,0)
+}
+
+$(document).ready(function(){
+    $.getJSON("data.json", function(data){
+        var airport_data = '';
+        $.each(data, function(key, value){
+            airport_data += '<tr>';
+            airport_data += '<td>'+value.id+'</td>';
+            airport_data += '<td>'+value.airport+'</td>';
+            airport_data += '<td>'+value.name+'</td>';
+            airport_data += '<td>'+value.municipality+'</td>';
+            airport_data += '<td>'+value.country+'</td>';
+            airport_data += '<td>'+value.elevation+'</td>';
+            airport_data += '<td>'+value.profit+'</td>';
+            airport_data += '<td>'+value.busyness+'</td>';
+            airport_data += '<td>'+value.ownership+'</td>'
+            airport_data += '</tr>';
+        });
+        $('#airports_table').append(airport_data);
+    });
+});
