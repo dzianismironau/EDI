1a97a26122457a63b83cec9517527bbecde9b67e
Michael Tryfanau <michael.tryfanau@gmail.com>
2024-01-06 21:21:19 +0100

    Finally got the pie chart working

--- script.js
+++ script.js
@@ -1,82 +1,70 @@
-//import 'chart.js';
-
 function top_back() {
     window.scrollTo(0,0)
-}
+};
 
 const API = "https://my.api.mockaroo.com/airports.json?key=b5ac6640";
+let private = 0;
+let public = 0;
+let mixed = 0;
 
-$(document).ready(function(){
-    $.getJSON('data.json', function(data){
+/*tablica + nabor peremennych*/
+$(document).ready(function () {
+    $.getJSON(API, function(data){
         var airport_data = '';
         var i = 0;
         $.each(data, function(key, value){
-            airport_data += '<tr>';
-            airport_data += '<td>'+value.id+'</td>';
-            airport_data += '<td>'+value.airport+'</td>';
-            airport_data += '<td>'+value.name+'</td>';
-            airport_data += '<td>'+value.municipality+'</td>';
-            airport_data += '<td>'+value.country+'</td>';
-            airport_data += '<td>'+value.elevation+'</td>';
-            airport_data += '<td>'+value.profit+'</td>';
-            airport_data += '<td>'+value.busyness+'</td>';
-            airport_data += '<td>'+value.ownership+'</td>'
-            airport_data += '</tr>';
-            i++;
-            if (i==10) {
-                return false;
+            if (i<=10) {
+                airport_data += '<tr>';
+                airport_data += '<td>'+value.id+'</td>';
+                airport_data += '<td>'+value.airport+'</td>';
+                airport_data += '<td>'+value.name+'</td>';
+                airport_data += '<td>'+value.municipality+'</td>';
+                airport_data += '<td>'+value.country+'</td>';
+                airport_data += '<td>'+value.elevation+'</td>';
+                airport_data += '<td>'+value.profit+'</td>';
+                airport_data += '<td>'+value.busyness+'</td>';
+                airport_data += '<td>'+value.ownership+'</td>'
+                airport_data += '</tr>';
             }
-        });
-        $('#airports_table').append(airport_data);
-    });
-});
+            i++;
+            console.log(value.ownership)
 
-$(document).ready(function(){
-    var priv = 0;
-    var pub = 0;
-    var mixed = 0;
-    $.getJSON('data.json', function(data){
-        $.each(data, function(key, value){
-            if (value.ownership === 'priv') {
-                priv += 1;
-            } else if (value.ownership === 'pub') {
-                pub += 1;
+            if (value.ownership === "private") {
+                private += 1;
+            } else if (value.ownership === "public") {
+                public += 1;
             } else {
                 mixed += 1;
-            }
-        })
-    })
+            };
+            console.log(1,value.ownership==='private',private)
+        });
+        $('#airports_table').append(airport_data);
+    }).then(() => {
+        Chart.defaults.interaction.mode = 'nearest';
 
-    const ctx = document.getElementById('pie_chart').getContext('2d');
-
-    const pieChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['priv', 'pub', 'mixed'],
+        let itemData = [public, private, mixed];
+        console.log(itemData);
+        const data = {
+            labels: ['Public', 'Private', 'Mixed'],
             datasets: [{
-                label: 'Ownership types',
-                data: [priv, pub, mixed],
+                label: 'Airport Ownership',
+                data: itemData,
                 backgroundColor: [
                     'rgb(255, 99, 132)',
                     'rgb(54, 162, 235)',
-                    'rgb(255, 205, 86)'
+                    'rgb(255, 206, 86)',
                 ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                ],
-                borderWidth: 1,
-    
-                hoverOffset: 4
             }]
-        }
-        }
-    )
-    $('#pie_chart').append(pieChart);
+        };
+
+        const config = {
+            type: 'pie',
+            data: data
+        };
+
+        const chart = new Chart(
+            document.getElementById('pie_chart'),
+            config
+        );
+    });
 });
-
-pieChart.canvas.parentNode.style.height = '800px';
-pieChart.canvas.parentNode.style.width = '800px';
-pieChart.canvas.parentNode.style.float = 'right';
-
