230c2b0bef1ef8c628ec131e932c895d1129f7b8
Michael Tryfanau <michael.tryfanau@gmail.com>
2024-01-07 16:20:38 +0100

    Changed the schema (reflected in the file), changed table, added the four charts
    Co-authored-by: dzianismironau <dzianismironau@users.noreply.github.com>

--- script.js
+++ script.js
@@ -7,9 +7,20 @@ let private = 0;
 let public = 0;
 let mixed = 0;
 
-/*tablica + nabor peremennych*/
+let smallProfit = 0;
+let mediumProfit = 0;
+let largeProfit = 0;
+
+let smallBusyness = 0;
+let mediumBusyness = 0;
+let largeBusyness = 0;
+let hugeBusyness = 0;
+
+let averageArray = [];
+let namesArray = [];
+
 $(document).ready(function () {
-    $.getJSON(API, function(data){
+    $.getJSON('data.json', function(data){
         var airport_data = '';
         var i = 0;
         $.each(data, function(key, value){
@@ -27,7 +38,7 @@ $(document).ready(function () {
                 airport_data += '</tr>';
             }
             i++;
-            console.log(value.ownership)
+            //console.log(value.ownership)
 
             if (value.ownership === "private") {
                 private += 1;
@@ -36,14 +47,34 @@ $(document).ready(function () {
             } else {
                 mixed += 1;
             };
-            console.log(1,value.ownership==='private',private)
+            //console.log(1,value.ownership==='private',private)
+            
+            if (value.profit < 30000000.) {
+                smallProfit++;
+            } else if (value.profit < 60000000.) {
+                mediumProfit++;
+            } else {
+                largeProfit++;
+            }
+
+            if (value.busyness < 10000000) {
+                smallBusyness++;
+            } else if (value.busyness < 40000000) {
+                mediumBusyness++;
+            } else if (value.busyness < 70000000) {
+                largeBusyness++;
+            } else {
+                hugeBusyness++;
+            }
+
+            namesArray.push(value.airport);
+            averageArray.push(value.profit/value.busyness);
         });
         $('#airports_table').append(airport_data);
     }).then(() => {
         Chart.defaults.interaction.mode = 'nearest';
 
         let itemData = [public, private, mixed];
-        console.log(itemData);
         const data = {
             labels: ['Public', 'Private', 'Mixed'],
             datasets: [{
@@ -59,12 +90,130 @@ $(document).ready(function () {
 
         const config = {
             type: 'pie',
-            data: data
+            data: data,
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                        labels: {
+                            color: 'white'
+                        }
+                    }
+                }
+            }
         };
 
         const chart = new Chart(
-            document.getElementById('pie_chart'),
+            document.getElementById('ownership_chart'),
             config
         );
+    }).then(() => {
+        let itemData = [smallProfit, mediumProfit, largeProfit];
+        const data = {
+            labels: ['Small', 'Medium', 'Large'],
+            datasets: [{
+                label: 'Airport Profits',
+                data: itemData,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 206, 86)',
+                ],
+            }]
+        };
+
+        const config = {
+            type: 'pie',
+            data: data,
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                        labels: {
+                            color: 'white'
+                        }
+                    }
+                }
+            }
+        };
+
+        const chart = new Chart(
+            document.getElementById('profit_chart'),
+            config
+        );
+        
+    }).then(() => {
+        let itemData = [smallBusyness, mediumBusyness, largeBusyness, hugeBusyness];
+        const data = {
+            labels: ['Small', 'Medium',  'Large', 'Huge'],
+            datasets: [{
+                label: 'Airport Profits',
+                data: itemData,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 206, 86)',
+                    'cyan'
+                ],
+            }]
+        };
+
+        const config = {
+            type: 'pie',
+            data: data,
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                        labels: {
+                            color: 'white'
+                        }
+                    },
+                    ChartjsPluginSorting
+                }
+            }
+        };
+
+        const chart = new Chart(
+            document.getElementById('busyness_chart'),
+            config
+        );
+        
+    }).then(() => {
+        let itemData = averageArray;
+        const data = {
+            labels: namesArray,
+            datasets: [{
+                label: 'Average profit per person visiting the airport',
+                data: itemData,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 206, 86)',
+                    'cyan'
+                ],
+            }]
+        };
+
+        const config = {
+            type: 'bar',
+            data: data,
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                        labels: {
+                            color: 'white'
+                        }
+                    }
+                }
+            }
+        };
+
+        const chart = new Chart(
+            document.getElementById('big_chart'),
+            config
+        );
+        
     });
 });
